cd60c9133abd77a2e6e82a103f4e7553d5fdf154
Giulio <giulio.marciante@hotmail.com>
2024-02-21 15:53:37 +0100

    first commit

---
+++ src/test/java/develhope/co/exercise11/Exercise11ApplicationTests.java
@@ -0,0 +1,13 @@
+package develhope.co.exercise11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
